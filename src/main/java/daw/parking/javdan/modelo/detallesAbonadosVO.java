831855dabff7488fa0b7ee42b9240cde381e8c24
DaniRM99 <danielraseromarin@gmail.com>
2019-05-16 14:12:40 +0200

    constructor de clase detallesAbonos

--- src/main/java/daw/parking/javdan/modelo/detallesAbonadosVO.java
+++ src/main/java/daw/parking/javdan/modelo/detallesAbonadosVO.java
@@ -11,6 +11,14 @@ public class detallesAbonadosVO {
     private int tipoAbono;
     private LocalDate feciniabono;
     private LocalDate fecfinabono;
+
+    public detallesAbonadosVO(int matricula, int codplaza, int tipoAbono, LocalDate feciniabono, LocalDate fecfinabono) {
+        this.matricula = matricula;
+        this.codplaza = codplaza;
+        this.tipoAbono = tipoAbono;
+        this.feciniabono = feciniabono;
+        this.fecfinabono = fecfinabono;
+    }
     
     
     
