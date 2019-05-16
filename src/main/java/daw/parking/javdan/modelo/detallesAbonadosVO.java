2317226f7db6751092245cd65bf23ecdc7310935
DaniRM99 <danielraseromarin@gmail.com>
2019-05-16 14:13:36 +0200

    Getters, Setters y toString de clase detallesAbonados

--- src/main/java/daw/parking/javdan/modelo/detallesAbonadosVO.java
+++ src/main/java/daw/parking/javdan/modelo/detallesAbonadosVO.java
@@ -19,6 +19,51 @@ public class detallesAbonadosVO {
         this.feciniabono = feciniabono;
         this.fecfinabono = fecfinabono;
     }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getCodplaza() {
+        return codplaza;
+    }
+
+    public void setCodplaza(int codplaza) {
+        this.codplaza = codplaza;
+    }
+
+    public int getTipoAbono() {
+        return tipoAbono;
+    }
+
+    public void setTipoAbono(int tipoAbono) {
+        this.tipoAbono = tipoAbono;
+    }
+
+    public LocalDate getFeciniabono() {
+        return feciniabono;
+    }
+
+    public void setFeciniabono(LocalDate feciniabono) {
+        this.feciniabono = feciniabono;
+    }
+
+    public LocalDate getFecfinabono() {
+        return fecfinabono;
+    }
+
+    public void setFecfinabono(LocalDate fecfinabono) {
+        this.fecfinabono = fecfinabono;
+    }
+
+    @Override
+    public String toString() {
+        return "detallesAbonadosVO{" + "matricula=" + matricula + ", codplaza=" + codplaza + ", tipoAbono=" + tipoAbono + ", feciniabono=" + feciniabono + ", fecfinabono=" + fecfinabono + '}';
+    }
     
     
     
