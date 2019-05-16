770f58e73bae304ad48d0164e65c874228f7e636
DaniRM99 <danielraseromarin@gmail.com>
2019-05-16 14:12:05 +0200

    Creación de clase detallesAbonados junto con sus atributos

---
+++ src/main/java/daw/parking/javdan/modelo/detallesAbonadosVO.java
@@ -0,0 +1,17 @@
+
+package daw.parking.javdan.modelo;
+
+import java.time.LocalDate;
+
+
+public class detallesAbonadosVO {
+   
+    private int matricula;
+    private int codplaza;
+    private int tipoAbono;
+    private LocalDate feciniabono;
+    private LocalDate fecfinabono;
+    
+    
+    
+}
