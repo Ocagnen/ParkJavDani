2bb4b4f9138b681f2f605a53910d12faa14b1bfa
DaniRM99 <danielraseromarin@gmail.com>
2019-05-16 14:08:53 +0200

    constructor de ticket

--- src/main/java/daw/parking/javdan/modelo/TicketVO.java
+++ src/main/java/daw/parking/javdan/modelo/TicketVO.java
@@ -14,6 +14,17 @@ public class TicketVO {
     private LocalDate fecsalida;
     private int pin;
     private double costeEstancia;
+
+    public TicketVO(int codticket, int tipovehi, int matricula, int codplaza, LocalDate fecingreso, LocalDate fecsalida, int pin, double costeEstancia) {
+        this.codticket = codticket;
+        this.tipovehi = tipovehi;
+        this.matricula = matricula;
+        this.codplaza = codplaza;
+        this.fecingreso = fecingreso;
+        this.fecsalida = fecsalida;
+        this.pin = pin;
+        this.costeEstancia = costeEstancia;
+    }
     
     
     
