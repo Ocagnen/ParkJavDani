bf1800d5b5d84252f9c6878d8456c921deb48493
DaniRM99 <danielraseromarin@gmail.com>
2019-05-16 14:07:39 +0200

    creacion de clase ticket y sus atributos

---
+++ src/main/java/daw/parking/javdan/modelo/TicketVO.java
@@ -0,0 +1,20 @@
+
+package daw.parking.javdan.modelo;
+
+import java.time.LocalDate;
+
+
+public class TicketVO {
+   
+    private int codticket;
+    private int tipovehi;
+    private int matricula;
+    private int codplaza;
+    private LocalDate fecingreso;
+    private LocalDate fecsalida;
+    private int pin;
+    private double costeEstancia;
+    
+    
+    
+}
