601002d10bf4ad5626663831ac7ccea14786a033
DaniRM99 <danielraseromarin@gmail.com>
2019-05-16 14:09:52 +0200

    Getters y Setters, toString de la clase Ticket

--- src/main/java/daw/parking/javdan/modelo/TicketVO.java
+++ src/main/java/daw/parking/javdan/modelo/TicketVO.java
@@ -25,6 +25,75 @@ public class TicketVO {
         this.pin = pin;
         this.costeEstancia = costeEstancia;
     }
+
+    public int getCodticket() {
+        return codticket;
+    }
+
+    public void setCodticket(int codticket) {
+        this.codticket = codticket;
+    }
+
+    public int getTipovehi() {
+        return tipovehi;
+    }
+
+    public void setTipovehi(int tipovehi) {
+        this.tipovehi = tipovehi;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getCodplaza() {
+        return codplaza;
+    }
+
+    public void setCodplaza(int codplaza) {
+        this.codplaza = codplaza;
+    }
+
+    public LocalDate getFecingreso() {
+        return fecingreso;
+    }
+
+    public void setFecingreso(LocalDate fecingreso) {
+        this.fecingreso = fecingreso;
+    }
+
+    public LocalDate getFecsalida() {
+        return fecsalida;
+    }
+
+    public void setFecsalida(LocalDate fecsalida) {
+        this.fecsalida = fecsalida;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public double getCosteEstancia() {
+        return costeEstancia;
+    }
+
+    public void setCosteEstancia(double costeEstancia) {
+        this.costeEstancia = costeEstancia;
+    }
+
+    @Override
+    public String toString() {
+        return "TicketVO{" + "codticket=" + codticket + ", tipovehi=" + tipovehi + ", matricula=" + matricula + ", codplaza=" + codplaza + ", fecingreso=" + fecingreso + ", fecsalida=" + fecsalida + ", pin=" + pin + ", costeEstancia=" + costeEstancia + '}';
+    }
     
     
     
