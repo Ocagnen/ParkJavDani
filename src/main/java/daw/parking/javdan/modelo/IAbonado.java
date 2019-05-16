9dc816658f627f291e2254a55e27a85e7f2899f0
Javier <ocagnen2@gmail.com>
2019-05-16 14:11:16 +0200

    Crea interface de Abonado con insert,update,..

---
+++ src/main/java/daw/parking/javdan/modelo/IAbonado.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.parking.javdan.modelo;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author javier
+ */
+public interface IAbonado {
+    
+     // Método para obtener todos los registros de la tabla
+    List<AbonadoVO> getAll() throws SQLException;
+    
+    // Méodo para obtener un registro a partir de la PK
+    AbonadoVO findByCod(int codabo) throws SQLException;
+    
+    // Método para insertar un registro
+    int insertAbonado (AbonadoVO abonado) throws SQLException;
+    
+    // Método para insertar varios registros
+    int insertAbonado (List<AbonadoVO> lista) throws SQLException;
+    
+    // Método para borrar una persona
+    int deleteAbonado (AbonadoVO p) throws SQLException;
+    
+    // Método para borrar toda la tabla
+    int deleteAbonado() throws SQLException;
+    
+    // Método para modificar un abonado
+    int updateAbonado (int codabo, AbonadoVO nuevosDatos) throws SQLException;
+    
+}
