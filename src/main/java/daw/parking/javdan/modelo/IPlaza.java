5c70f18fc00a6831f2a392ab4378073bd037ced6
Javier <ocagnen2@gmail.com>
2019-05-16 14:27:12 +0200

    Crea interface con de Plaza con insert, update, ..

---
+++ src/main/java/daw/parking/javdan/modelo/IPlaza.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.parking.javdan.modelo;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author javier
+ */
+public interface IPlaza {
+    
+    // Método para obtener todos los registros de la tabla
+    List<PlazaVO> getAll() throws SQLException;
+    
+    // Méodo para obtener un registro a partir de la PK
+    PlazaVO findByCod(int codplaz) throws SQLException;
+    
+    // Método para insertar un registro
+    int insertPlaza (PlazaVO plaza) throws SQLException;
+    
+    // Método para insertar varios registros
+    int insertPlaza (List<PlazaVO> lista) throws SQLException;
+    
+    // Método para borrar una persona
+    int deletePlaza (PlazaVO p) throws SQLException;
+    
+    // Método para borrar toda la tabla
+    int deletePlaza() throws SQLException;
+    
+    // Método para modificar un abonado
+    int updatePlaza (int codpla, PlazaVO nuevosDatos) throws SQLException;
+    
+}
