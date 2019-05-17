0b29935716c9c650d6eca7f34fdd83f215298d34
DaniRM99 <danielraseromarin@gmail.com>
2019-05-17 12:20:57 +0200

    Creación interfaz DetalleAbonadoVO

---
+++ src/main/java/daw/parking/javdan/modelo/IDetalleAbonado.java
@@ -0,0 +1,35 @@
+
+package daw.parking.javdan.modelo;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+
+public interface IDetalleAbonado {
+    
+         // Método para obtener todos los registros de la tabla
+    List<DetalleAbonadoVO> getAll() throws SQLException;
+    
+    // Méodo para obtener un registro a partir de la PK
+    PlazaVO findByCod(int matricula,LocalDate fecIniabono,int codplaz) throws SQLException;
+    
+    // Método para insertar un registro
+    int insertDetAb (DetalleAbonadoVO DetalleAbonado) throws SQLException;
+    
+    // Método para insertar varios registros
+    int insertDetAb (List<DetalleAbonadoVO> lista) throws SQLException;
+    
+    // Método para borrar una persona
+    int deleteDetAb (DetalleAbonadoVO d) throws SQLException;
+    
+    // Método para borrar toda la tabla
+    int deleteDetAb() throws SQLException;
+    
+    // Método para modificar un abonado
+    int updateDetAb (int matricula,LocalDate fecIniabono,int codplaz, DetalleAbonadoVO nuevosDatos) throws SQLException;
+    
+}
+
+    
+
