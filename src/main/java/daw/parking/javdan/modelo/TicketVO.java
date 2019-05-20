187ecb32dc8b8b7520938f3a6bc2233eab40542e
Javier <ocagnen2@gmail.com>
2019-05-20 10:31:53 +0200

    Añade equals y hash code a Ticket

--- src/main/java/daw/parking/javdan/modelo/TicketVO.java
+++ src/main/java/daw/parking/javdan/modelo/TicketVO.java
@@ -2,6 +2,7 @@
 package daw.parking.javdan.modelo;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 
 public class TicketVO {
@@ -106,6 +107,61 @@ public class TicketVO {
     public String toString() {
         return "TicketVO{" + "codticket=" + codTicket + ", tipovehi=" + tipoVehi + ", matricula=" + matricula + ", codplaza=" + codPlaza + ", fecingreso=" + fecIngreso + ", fecsalida=" + fecSalida + ", pin=" + pin + ", costeEstancia=" + costeEstancia + '}';
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + this.codTicket;
+        hash = 29 * hash + this.tipoVehi;
+        hash = 29 * hash + Objects.hashCode(this.matricula);
+        hash = 29 * hash + this.codPlaza;
+        hash = 29 * hash + Objects.hashCode(this.fecIngreso);
+        hash = 29 * hash + Objects.hashCode(this.fecSalida);
+        hash = 29 * hash + this.pin;
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.costeEstancia) ^ (Double.doubleToLongBits(this.costeEstancia) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TicketVO other = (TicketVO) obj;
+        if (this.codTicket != other.codTicket) {
+            return false;
+        }
+        if (this.tipoVehi != other.tipoVehi) {
+            return false;
+        }
+        if (this.codPlaza != other.codPlaza) {
+            return false;
+        }
+        if (this.pin != other.pin) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.costeEstancia) != Double.doubleToLongBits(other.costeEstancia)) {
+            return false;
+        }
+        if (!Objects.equals(this.matricula, other.matricula)) {
+            return false;
+        }
+        if (!Objects.equals(this.fecIngreso, other.fecIngreso)) {
+            return false;
+        }
+        if (!Objects.equals(this.fecSalida, other.fecSalida)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
     
     
