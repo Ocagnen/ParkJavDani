d4e1ea4044a9fc459b4a0df05436aa8b44e4f5b4
DaniRM99 <danielraseromarin@gmail.com>
2019-05-21 14:42:47 +0200

    comentario2

--- src/main/java/daw/parking/javdan/modelo/Conexion.java
+++ src/main/java/daw/parking/javdan/modelo/Conexion.java
@@ -21,6 +21,11 @@ public class Conexion {
     private static final String USER = "javier";
     private static final String PASS = "contraseña";
     
+    /*private static final String SERVIDOR = "jdbc:mysql://192.168.56.102/";
+    private static final String NOMBRE_BASE_DATOS = "parking";
+    private static final String USER = "root";
+    private static final String PASS = "123456";*/
+    
     private static Connection instancia = null;
     
     // Constructor privado no accesible desde otras clases
