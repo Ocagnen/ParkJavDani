977395d6d8279385e45e20019ed2575e14a8e20e
DaniRM99 <danielraseromarin@gmail.com>
2019-05-23 14:37:59 +0200

    Avance en el método de zonaCliente

--- src/main/java/daw/praking/javdan/programa/Vista.java
+++ src/main/java/daw/praking/javdan/programa/Vista.java
@@ -1,96 +1,101 @@
-
 package daw.praking.javdan.programa;
 
 import java.util.Scanner;
 
-
 public class Vista {
-    
-    public void menu(){
-        
+
+    public void menu() {
+
         Scanner teclado = new Scanner(System.in);
-        
+
         System.out.println("Desea acceder a zona cliente o zona administrador");
         System.out.println("Escoja una opción de las siguientes");
         System.out.println("1-->Zona administrador");
         System.out.println("2-->Zona cliente");
-        
+
         System.out.println("-----------------");
         System.out.println("-----------------");
         // Cambialo por un while
-        int eleccionZona=0;
-                
-        while(!(eleccionZona==1 ||eleccionZona==2)){
-        
-        eleccionZona= teclado.nextInt();
-        
+        int eleccionZona = teclado.nextInt();
+
+        while (!(eleccionZona == 1 || eleccionZona == 2)) {
+
+            System.out.println("ERROR, seleccione una de las opciones mostradas");
+            eleccionZona = teclado.nextInt();
+
         }
-       
+
         switch (eleccionZona) {
             case 1:
-                
+
                 System.out.println("Ha elegido zona administrador");
                 System.out.println("-----------------");
                 break;
-                
-            case 2: 
-               
+
+            case 2:
+
                 System.out.println("Ha elegido zona cliente");
                 System.out.println("-----------------");
                 break;
-                
-        }
-        }
-    
-        public void seleccionCliente(){
-        
-            Scanner teclado = new Scanner(System.in);
-            
-        System.out.println("¿Que desea hacer?: ");
-        System.out.println("1-->Depositar Vehiculo");
-        System.out.println("2-->Retirar Vehiculo");
-        System.out.println("3-->Depositar Abonado");
-        System.out.println("4-->Retirar Abonado");
-        
-        
-        System.out.println("-----------------");
-        System.out.println("-----------------");
-        
-        int seleccionAccion=0;
-        
-        while (!(seleccionAccion==1 ||seleccionAccion==2 |seleccionAccion==3 ||seleccionAccion==4)){
-            
-            seleccionAccion= teclado.nextInt();
-            
-        }
-        
-            switch (seleccionAccion) {
-                case 1:
-                    
-                    System.out.println("Ha elegido Depositar Vehiculo");
-                    System.out.println("-----------------");
-                    break;
-                    
-                case 2:
-                    
-                    System.out.println("Ha elegido Retirar Vehiculo");
-                    System.out.println("-----------------");
-                    break;
-                    
-                case 3:
-                    
-                    System.out.println("Ha elegido Depositar Abonado");
-                    System.out.println("-----------------");
-                    break;
-                    
-                case 4:
-                    
-                    System.out.println("Ha elegido Retirar Abonado");
-                    System.out.println("-----------------");
-                    break;
-                
-            }
-        
+
         }
     }
-    
+
+    public void seleccionCliente() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("¿Es usted abonado?: ");
+        System.out.println("1-->Si");
+        System.out.println("2-->No");
+        System.out.println("-----------------");
+        System.out.println("-----------------");
+
+        int seleccionAccion = teclado.nextInt();
+
+        while (!(seleccionAccion == 1 || seleccionAccion == 2)) {
+            System.out.println("ERROR, seleccione una de las opciones mostradas");
+            seleccionAccion = teclado.nextInt();
+        }
+
+        switch (seleccionAccion) {
+            case 1:
+                
+                System.out.println("Seleccione una opción: ");
+                System.out.println("1-->Depositar vehículo");
+                System.out.println("2-->Retirar vehículo");
+                System.out.println("3-->Retirar abono");
+                seleccionAccion = teclado.nextInt();
+                break;
+
+            case 2:
+
+                System.out.println("¿Desea hacerse abonado?");
+                System.out.println("1-->Si");
+                System.out.println("2-->No");
+                seleccionAccion = teclado.nextInt();
+
+                while (!(seleccionAccion == 1 || seleccionAccion == 2)) {
+                    System.out.println("ERROR, seleccione una de las opciones mostradas");
+                    seleccionAccion = teclado.nextInt();
+                }
+
+                switch (seleccionAccion) {
+                    case 1:
+                        
+                        System.out.println("Ha entrado en el menú de Depositar Abonado");
+                        break;
+
+                    case 2:
+                        
+                        System.out.println("Elija una opción: ");
+                        System.out.println("1-->Depositar vehículo");
+                        System.out.println("2-->Retirar vehículo");    
+                        seleccionAccion = teclado.nextInt();
+                        break;
+                }
+                break;
+        }
+
+    }
+}
